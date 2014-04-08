2273e604947f7ac86762f18f82217454b6a23f0d
terryjwoods <tjwoods@syr.edu>
2014-04-08 15:50:24 -0400

    Mainmenu etc

---
+++ 4Eyes/Assets/Standard Assets/Scripts/General Scripts/MovingPlatforms.js
@@ -0,0 +1,47 @@
+﻿#pragma strict
+private var Xpos : float;
+private var Ypos : float;
+private var max : boolean;
+var Vert : boolean;
+var maxAmount : float;
+var step : float;
+
+function Start () {
+	Xpos = transform.position.x;
+	Ypos = transform.position.y;
+	max = false;
+}
+
+function Update () {
+	//SET THE MAX
+	if(Vert){ //Vertical
+		if(transform.position.y >= Ypos + maxAmount){
+			max = true;
+		} else if(transform.position.y <= Ypos){
+			max = false;
+		}
+	} 
+	else { //horizontal
+		if(transform.position.x >= Xpos + maxAmount) {
+			max = true;
+		} else if(transform.position.x <= Xpos) {
+			max = false;
+		}
+	}
+	
+	//MOVING THE PLATFORM
+	if(Vert){ // Vertical movement
+		if(!max){
+			transform.position.y += step;
+		} else {
+			transform.position.y -= step;
+		}
+	} else { //Horizontal movement
+		if(!max){
+			transform.position.x += step;
+		} else {
+			transform.position.x -= step;
+		}
+	}
+}
+
---
+++ 4Eyes/Assets/Standard Assets/Scripts/General Scripts/QuitButton.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+var quitNormal : Texture2D;
+var quitHover : Texture2D;
+
+function Start () {
+
+}
+
+function OnMouseEnter () {
+
+     guiTexture.texture = quitHover; 
+
+}
+
+function OnMouseExit () {
+  
+     guiTexture.texture = quitNormal;
+
+}
+
+function OnMouseDown () {
+
+     Application.Quit();
+
+}
