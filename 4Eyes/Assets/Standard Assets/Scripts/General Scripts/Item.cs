db2a3b0849348ede60dffb8a51410638af1c5342
terryjwoods <tjwoods@syr.edu>
2014-04-11 11:26:37 -0400

    Items and Inventory Manager
    Must create an empty object to use inventory manager

---
+++ 4Eyes/Assets/Standard Assets/Scripts/General Scripts/Item.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Item : ScriptableObject {
+
+	public int cost = 0;
+	public string name = " " ;
+	public string description = " ";
+	
+}
+
+[System.Serializable]
+public class Weapon : Item {
+	public int damage = 0;
+        
+}
+
+[System.Serializable]
+public class Armor : Item {
+	public int denfense = 0; 
+}
---
+++ 4Eyes/Assets/Standard Assets/Scripts/General Scripts/ItemManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemManager : MonoBehaviour {
+public List<Item> inventory = new List<Item>();
+private string newItemName = " ";
+private string newItemCost = " ";
+private string newItemDescription = " "; 
+
+	void OnGUI(){
+
+		newItemName = GUILayout.TextField(newItemName);
+		newItemCost = GUILayout.TextField(newItemCost);
+	    newItemDescription = GUILayout.TextField(newItemDescription);
+
+		if(GUILayout.Button("Add New Item")){
+
+			Item newItem = ScriptableObject.CreateInstance<Item>();
+			newItem.name = newItemName;
+			newItem.cost = System.Convert.ToInt32(newItemCost);
+			newItem.description = newItemDescription; 
+
+			inventory.Add(newItem);
+
+		}
+
+		if(GUILayout.Button("Add New Weapon")){
+			
+			Item newItem = ScriptableObject.CreateInstance<Weapon>();
+			newItem.name = newItemName;
+			newItem.cost = System.Convert.ToInt32(newItemCost);
+			newItem.description = newItemDescription; 
+			
+			inventory.Add(newItem);
+				
+		}
+
+		for(int i = 0; 1 < inventory.Count; i++){
+
+			GUILayout.Label(inventory[i].name + " - " + 
+			                inventory[i].description + " _ " +
+			                inventory[i].cost + " _ " +
+			                inventory[i].GetType());
+	    
+        }
+	
+	                                                           
+	}
+}
+
+
+
